8b2b49fe188e725457a10129058f18821b8995e1
rohanghosh <rohangho@live.com>
2023-09-09 12:46:11 +0530

    Added the three repo to check dependency resolution

---
+++ MyApplication/lib/src/main/java/com/example/lib/MyClassAbc.java
@@ -0,0 +1,13 @@
+package com.example.lib;
+
+import com.example.mybasejava.MyClass;
+
+public class MyClassAbc {
+
+    public static void callMeInternal()
+    {
+        MyClass.callMe();
+        System.out.println("I am internal");
+
+    }
+}
---
+++ StackQuestion/myBaseJava/src/main/java/com/example/mybasejava/MyClass.java
@@ -0,0 +1,10 @@
+package com.example.mybasejava;
+
+public class MyClass {
+
+    public static void callMe()
+    {
+        System.out.println("I am version 2");
+    }
+
+}
